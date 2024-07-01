23d6bf2c5f185ea8093d17f8da12c2f57a25a932
shiyangzhang <shiyangzhang@whale.im>
2024-07-01 16:29:42 +0800

    [HEAD]

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int array[] = {1, 7, 4, 5, 9, 3, 5, 11, 6, 3, 4};
+    int i = 0;
+
+    while (i < 10)
+    {
+        /* your code goes here */
+        if (array[i] < 5)
+        {
+            i++;
+            continue;
+        }
+        if (array[i] > 10)
+        {
+            break;
+        }
+        printf("%d\n", array[i]);
+        i++;
+    }
+
+    return 0;
+}
---
+++ note.md
@@ -0,0 +1,6 @@
+https://www.learn-c.org/en/Functions
+
+## 2024-07-01 15:13:10
+
+- @ques c 字符串拼接
+- @ques c print char
